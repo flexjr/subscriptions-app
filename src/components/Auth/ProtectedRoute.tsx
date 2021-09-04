8dc927dd3d57b24124e1c11b42d420ac128d1a2f
Goi Jia Jian <jiajiannn@gmail.com>
2021-09-04 22:51:46 +0800

    feat: annoying ProtectedRoute not working

--- src/components/Auth/ProtectedRoute.tsx
+++ src/components/Auth/ProtectedRoute.tsx
@@ -1,8 +1,17 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import React from "react";
 import { Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export const ProtectedRoute = ({ component, ...args }: React.PropsWithChildren<any>) => {
+  const { isAuthenticated } = useAuth0();
+  const history = useHistory();
+
+  // TODO: By right the withAuthenticationRequired should redirect to Auth0 but somehow it's not working, so this is just a quickfix
+  if (!isAuthenticated) {
+    history.push("/");
+  }
+
   return (
     <Route
       render={(props) => {
