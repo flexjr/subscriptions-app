3900ac7e90c4dc910ff2a41e95494728b84d3a48
Goi Jia Jian <jiajiannn@gmail.com>
2021-10-04 00:08:22 +0800

    chore: headless https

--- integration/integration.spec.ts
+++ integration/integration.spec.ts
@@ -2,14 +2,13 @@ import { Selector } from "testcafe";
 
 fixture("Login Page (/)").page`https://localhost:3000`;
 
-const LoginTitle = Selector("Title").nth(0);
+const PageTitle = Selector("title").nth(0);
 const LoginButton = Selector("Button").nth(0);
 const BuildLabel = Selector("p").nth(0);
 
 test("Login page is rendered correctly", async (t) => {
-  await t.click(LoginButton);
-
-  await t.expect(LoginTitle.textContent).eql("Welcome back to Flex!");
-  await t.expect(LoginButton.textContent).eql("Welcome back to Flex!");
+  await t.expect(PageTitle.textContent).eql("Flex Jr");
+  await t.expect(LoginButton.textContent).eql("Login");
   await t.expect(BuildLabel.textContent).contains("Build v1.0.0-");
+  // await t.click(LoginButton);
 });
