e4df56e475e8fb98d08bd58c639c5cbd05f7d91b
Goi Jia Jian <jiajiannn@gmail.com>
2021-10-21 02:33:52 +0800

    feat: renew sub

--- src/hooks/useFlex.tsx
+++ src/hooks/useFlex.tsx
@@ -1,9 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Tag, Tooltip } from "antd";
+import React from "react";
 import { useEffect, useState } from "react";
 import { API_URL, AUTH0_API_AUDIENCE, getData } from "../shared";
 import { FlexUserInfo } from "../types";
 
-export const useFlex = (): { flexUserInfo; isLoadingFlexUserInfo; isOnboarded; isLoadingOnboarded; setIsOnboarded } => {
+export const useFlex = (): {
+  flexUserInfo;
+  isLoadingFlexUserInfo;
+  isOnboarded;
+  isLoadingOnboarded;
+  setIsOnboarded;
+  subscriptionPlanFriendlyName;
+  subscriptionPlanStatus;
+} => {
   const [isLoadingOnboarded, setIsLoadingOnboarded] = useState(true);
   const [isOnboarded, setIsOnboarded] = useState<boolean | undefined>(undefined);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -77,5 +87,57 @@ export const useFlex = (): { flexUserInfo; isLoadingFlexUserInfo; isOnboarded; i
     getFlexUserInfo();
   }, [getAccessTokenSilently, isAuthenticated]);
 
-  return { flexUserInfo, isLoadingFlexUserInfo, isOnboarded, isLoadingOnboarded, setIsOnboarded };
+  const subscriptionPlanFriendlyName = (subscriptionPlan: string): string | undefined => {
+    const parts = subscriptionPlan.split("-"); // e.g. FLEX_PRO-SGD-Monthly
+    const plan = parts[0];
+    // const currency = parts[1];
+    const frequency = parts[2];
+
+    let formattedPlanName = "";
+    switch (plan) {
+      case "FLEX_STARTER":
+        return "Flex Starter";
+      case "FLEX_PRO":
+        formattedPlanName = `Flex Pro`;
+        return `${formattedPlanName} (${frequency})`;
+      case "FLEX_PREMIUM":
+        formattedPlanName = `Flex Premium`;
+        return `${formattedPlanName} (${frequency})`;
+      default:
+        return undefined;
+    }
+  };
+
+  const subscriptionPlanStatus = (status: string): JSX.Element | undefined => {
+    switch (status) {
+      case "active":
+        return <Tag color="#3dbd7d">Active</Tag>;
+      case "in_trial":
+        return <Tag color="#00a854">Trial</Tag>;
+      case "non_renewing":
+        return (
+          <Tooltip placement="top" title="Subscription is still active, but will not be renewed when it ends.">
+            <Tag color="#ffbf00">Non Renewing</Tag>
+          </Tooltip>
+        );
+      case "cancelled":
+        return <Tag color="#f04134">Cancelled</Tag>;
+      case "future":
+        return <Tag>Future</Tag>;
+      case "paused":
+        return <Tag>Paused</Tag>;
+      default:
+        return undefined;
+    }
+  };
+
+  return {
+    flexUserInfo,
+    isLoadingFlexUserInfo,
+    isOnboarded,
+    isLoadingOnboarded,
+    setIsOnboarded,
+    subscriptionPlanFriendlyName,
+    subscriptionPlanStatus,
+  };
 };
