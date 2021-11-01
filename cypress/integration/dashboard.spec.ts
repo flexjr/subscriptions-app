4f81be13524fee431a75654f8fc0507d6a1a31db
jeremytanes <jeremy.tan.2019@sis.smu.edu.sg>
2021-11-01 12:01:22 +0800

    test: dashboard test [WIP]

---
+++ cypress/integration/dashboard.spec.ts
@@ -0,0 +1,30 @@
+export {};
+
+describe("Dashboard", () => {
+  it("Should login with valid credentials", () => {
+    cy.visit("/");
+    cy.login().then(({ body: { access_token, expires_in, id_token, token_type } }) => {
+      cy.visit("/flex/dashboard");
+      cy.window().then((win) => {
+        win.localStorage.setItem(
+          `@@auth0spajs@@::${Cypress.env("auth_client_id")}::${Cypress.env("auth_audience")}::${Cypress.env(
+            "auth_scope"
+          )} offline_access`, // Could also be ::default:: instead of ::${audience}::
+          JSON.stringify({
+            body: {
+              client_id: Cypress.env("auth_client_id"),
+              access_token,
+              id_token,
+              scope: Cypress.env("auth_scope"),
+              expires_in,
+              token_type,
+              decodedToken: { user: JSON.parse(Buffer.from(id_token.split(".")[1], "base64").toString("ascii")) },
+              audience: Cypress.env("auth_audience"),
+            },
+            expiresAt: Math.floor(Date.now() / 1000) + expires_in,
+          })
+        );
+      });
+    });
+  });
+});
