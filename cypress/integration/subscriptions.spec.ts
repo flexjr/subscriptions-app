2587a353550051ff5555b7231fc52d36526c9356
Goi Jia Jian <jiajiannn@gmail.com>
2021-11-13 21:27:20 +0800

    test: fix typo

--- cypress/integration/subscriptions.spec.ts
+++ cypress/integration/subscriptions.spec.ts
@@ -71,7 +71,7 @@ describe("Subscriptions Page", () => {
     cy.get("[data-cy=saved-card]").should("exist").contains("visa");
     cy.get("[data-cy=saved-card-last4]").should("exist").contains("•••• 1111");
     cy.get("[data-cy=upgrade-plan]").should("exist").contains("Flex Pro (Yearly)");
-    cy.get("[data-cy=upgrade-price]").should("exist").contains("SGD 89.99");
+    cy.get("[data-cy=upgrade-unit-price]").should("exist").contains("SGD 89.99");
     cy.get("[data-cy=upgrade-list-1250]").should("exist").contains("e2e-testing@example.com");
     cy.get("[data-cy=upgrade-tncs]").should("exist").contains("You will be charged SGD 89.99/year immediately");
   });
